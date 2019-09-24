6a2db3ab99fa5b0eddff3899cfea70d258378f19
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-24 03:04:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  
+  attr_accessor :name, :student, :grade
+  
+  ROSTER = []
+  
+  def initialzie(name)
+    @name = name 
+    roster
+  end
+  
+  def roster 
+    ROSTER
+  end
+  
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    
+    if ROSTER.include?(grade)
+      ROSTER.grade << student
+    else
+      ROSTER << {grade: [student]}
+    end
+  end
+  
+  def sort 
+    
+  end
+  
+end
