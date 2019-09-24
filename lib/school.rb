a9827a4780b504ddb6d4154306b4298c5652fd2b
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-24 03:36:11 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster= {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if !@roster.key?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+  sort_hash = {}
+  @roster.each { |grade, name| 
+    sort_hash[grade] = name.sort }
+    
+    return sort_hash
+  end
+  
+end
+
+hodgesbend= School.new("Hodges bend")
+hodgesbend.add_student("Nina Kazz", 9) 
+puts hodgesbend.grade(9)
